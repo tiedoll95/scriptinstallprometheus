22da5fdc038fa0f76ac72247959cb95c9bd78875
tiedoll95 <52814193+tiedoll95@users.noreply.github.com>
2020-05-05 20:42:12 +0700

    Create install_snmp_exporter.sh

---
+++ install_snmp_exporter.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+yum install wget -y
+##remove user and prometheus_old
+systemctl stop firewalld
+systemctl disable firewalld
+sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
+systemctl stop node_exporter
+systemctl stop prometheus
+
+rm -rf /home/prometheus/
+
+userdel prometheus
+
+
+### tao user prometheus 
+
+useradd -m -s /bin/bash prometheus
+
+sleep 1
+
+cd /home/prometheus/
+wget https://github.com/prometheus/snmp_exporter/releases/download/v0.17.0/snmp_exporter-0.17.0.linux-amd64.tar.gz
+
+tar -xzf snmp_exporter-0.17.0.linux-amd64.tar.gz
+
+
+mv snmp_exporter-0.17.0.linux-amd64.tar.gz snmp_exporter
+
+cd /etc/systemd/system/
+
+cat << EOF > node_exporter.service
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+[Service]
+User=prometheus
+ExecStart=/home/prometheus/node_exporter/node_exporter --config.file /home/prometheus/snmp_exporter/snmp.yml
+[Install]
+WantedBy=default.target
+EOF
+
+chown prometheus /home/prometheus/snmp_exporter
+systemctl daemon-reload
+systemctl restart snmp_exporter
+systemctl enable snmp_exporter
